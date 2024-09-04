7dd6488cc026e05595e1c5accee9237103187d89
Andrew R <beta@runelite.bot>
2024-09-04 11:00:14 -0500

    add favicon to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,16 @@
 	});
 </script>
 
+<svelte:head>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+	<meta name="msapplication-TileColor" content="#7480ff" />
+	<meta name="theme-color" content="#333333" />
+</svelte:head>
+
 {#if mounted}
 	<div class="flex flex-col min-h-screen overflow-hidden">
 		<DotPattern fillColor="rgb(163 163 163 / 0.15)" class="flex-grow flex flex-col">
