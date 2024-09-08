fa66763ad22f13a1c91ab3dfb3109d1c4e2e12f6
Andrew R <beta@runelite.bot>
2024-09-08 09:04:29 -0700

    update opengraph image

--- src/components/utility/Head.svelte
+++ src/components/utility/Head.svelte
@@ -6,23 +6,24 @@
   <title>{$page.data.title} | rubes.dev</title>
   <meta name="description" content={$page.data.description} />
   <meta property="og_site_name" content="Andrew Rubenstein - Frontend Developer" />
-  <meta property="og:url" content="https://www.rubes.dev{$page.url.pathname.toString()}" />
+  <meta property="og:url" content="https://rubes.dev{$page.url.pathname.toString()}" />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={$page.data.title} />
   <meta property="og:description" content={$page.data.description} />
-  <meta property="og:image" content="https://www.rubes.dev/android-chrome-512x512.png" />
+  <meta property="og:image" content="https://rubes.dev/opengraph-image.jpg" />
   <link rel="icon" href={$page.data.icon.href} />
 
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content="rubes.dev" />
-  <meta property="twitter:url" content="https://www.rubes.dev{$page.url.pathname.toString()}" />
+  <meta property="twitter:url" content="https://rubes.dev{$page.url.pathname.toString()}" />
+  <meta property="twitter:image" content="https://rubes.dev/opengraph-image.jpg" />
   <meta name="twitter:title" content={$page.data.title} />
   <meta name="twitter:description" content={$page.data.description} />
   {@html `  <script type="application/ld+json">{
      "@context": "https://schema.org",
      "@type": "Website",
      "name": "${$page.data.title} | rubes.dev",
-     "url": "https//www.rubes.dev${$page.url.pathname}",
-     "logo": "https://www.rubes.dev/android-chrome-512x512.png"
+     "url": "https//rubes.dev${$page.url.pathname}",
+     "logo": "https://rubes.dev/android-chrome-512x512.png"
      }</script>`}
 </svelte:head>
