0ca8a79b83d991c7f9e9aa2b5a99dc9ff254c9b4
Andrew R <beta@runelite.bot>
2024-09-08 09:07:18 -0700

    update description

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,7 @@ export const ssr = true;
 export function load() {
     return {
         title: "Andrew Rubenstein - Frontend Developer",
-        description: "Take a stroll through the digital garden cared for by Andrew Rubenstein - you're friendly neighborhood frontend engineer.",
+        description: "Take a stroll through Andrew's digital garden and enjoy the ramblings of your friendly neighborhood frontend engineer.",
         icon: { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
        
     }
