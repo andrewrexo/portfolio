b8f4bd4d35530fd7121b12c0327db1846887cd13
Andrew R <beta@runelite.bot>
2024-09-20 14:09:23 -0500

    add mdsvex for markdown processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import image from 'svelte-image';
+import { mdsvex } from 'mdsvex';
+
+const mdsvexOptions = {
+  extensions: ['.md'],
+}
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sequence([vitePreprocess(), image(), preprocessMeltUI()]),
-	kit: {
+	preprocess: sequence([vitePreprocess(), mdsvex(mdsvexOptions), preprocessMeltUI()]),
+	extensions: ['.svelte', '.md'],
+  kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
