93fdaefd30c6ed0ad432e6bae2590e4d8411fe25
Andrew R <beta@runelite.bot>
2024-11-04 15:41:49 -0500

    update meta description

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,10 +3,10 @@ export const ssr = true;
 
 // Add SEO metadata
 export function load() {
-    return {
-        title: "Andrew Rubenstein - Frontend Developer",
-        description: "Take a stroll through Andrew's digital garden and enjoy the ramblings of your friendly neighborhood frontend engineer.",
-        icon: { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-       
-    }
+  return {
+    title: 'Andrew R - FS Engineer',
+    description:
+      'From pixels to performance, I strive to make the web more wonderful. Looking for someone to build your next project? Drop me a line.',
+    icon: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }
+  };
 }
