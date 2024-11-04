0d97a7a9bc1bdf9d6f131b3b30b1829568b7bb1d
Andrew R <beta@runelite.bot>
2024-11-04 14:49:20 -0500

    add missing build crawl entires + types

--- svelte.config.js
+++ svelte.config.js
@@ -4,38 +4,52 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 const mdsvexOptions = {
-  extensions: ['.md'],
-}
+  extensions: ['.md']
+};
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: sequence([vitePreprocess(), mdsvex(mdsvexOptions), preprocessMeltUI()]),
-	extensions: ['.svelte', '.md'],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: sequence([vitePreprocess(), mdsvex(mdsvexOptions), preprocessMeltUI()]),
+  extensions: ['.svelte', '.md'],
   kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		}),
-		alias: {
-			$components: './src/components',
-			$lib: './src/lib',
-			$routes: './src/routes',
-			$styles: './src/styles',
-			$types: './src/types',
-			$utils: './src/utils'
-		}
-	}
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      },
+      platformProxy: {
+        environment: undefined,
+        experimentalJsonConfig: false,
+        persist: false
+      }
+    }),
+    alias: {
+      $components: './src/components',
+      $lib: './src/lib',
+      $projects: './src/projects',
+      $routes: './src/routes',
+      $styles: './src/styles',
+      $types: './src/types',
+      $utils: './src/utils'
+    },
+    prerender: {
+      crawl: true,
+      entries: [
+        '/projects/opentxpacker',
+        '/projects/milkshake',
+        '/projects/mappingtool',
+        '/projects/opentxpacker',
+        '/projects/',
+        '/post',
+        '/',
+        '/post/debug-post'
+      ]
+    }
+  }
 };
 export default config;
