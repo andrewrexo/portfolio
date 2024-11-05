3c50c38ad5d660835d9189f7e7439fb5066e3342
Andrew R <beta@runelite.bot>
2024-11-05 11:25:18 -0500

    extract function from project load +server

--- src/lib/project.ts
+++ src/lib/project.ts
@@ -5,3 +5,40 @@ export type Project = {
   slug: string;
   published?: boolean;
 };
+
+export const loadProjectsFromDisk = async () => {
+  const projectFiles = import.meta.glob('/src/projects/*.md');
+
+  return Promise.all(
+    Object.entries(projectFiles).map(async ([path, loader]) => {
+      const project = await loader();
+      const slug = path.split('/').pop()?.replace('.md', '');
+
+      if (project && typeof project === 'object' && 'metadata' in project && 'default' in project) {
+        const metadata = project.metadata as Omit<Project, 'slug'>;
+
+        return {
+          slug,
+          ...metadata,
+          content: project.default
+        };
+      }
+    })
+  );
+};
+
+export const loadProjectFromDisk = async (slug: string) => {
+  const projectPath = `/src/projects/${slug}.md`;
+  const project = await import(projectPath);
+
+  if (project && typeof project === 'object' && 'metadata' in project && 'default' in project) {
+    const metadata = project.metadata as Omit<Project, 'slug'>;
+
+    return {
+      slug,
+      ...metadata,
+      content: project.default
+    };
+  }
+  return null; // Return null if no project is found
+};
--- src/routes/get/projects/+server.ts
+++ src/routes/get/projects/+server.ts
@@ -1,26 +1,9 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import type { Project } from '$lib/project';
+import { loadProjectsFromDisk } from '$lib/project';
 
 export const GET: RequestHandler = async () => {
-  const projectFiles = import.meta.glob('/src/projects/*.md');
-
-  const projects = await Promise.all(
-    Object.entries(projectFiles).map(async ([path, loader]) => {
-      const project = await loader();
-      const slug = path.split('/').pop()?.replace('.md', '');
-
-      if (project && typeof project === 'object' && 'metadata' in project && 'default' in project) {
-        const metadata = project.metadata as Omit<Project, 'slug'>;
-
-        return {
-          slug,
-          ...metadata,
-          content: project.default
-        };
-      }
-    })
-  );
+  const projects = await loadProjectsFromDisk();
 
   return json(projects);
 };
